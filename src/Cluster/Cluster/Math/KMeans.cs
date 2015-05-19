716bf0a39179a6b02eaf5cbc6cacf6bfac1868fa
Jakub-Ciecierski <jakub.ciecier@gmail.com>
2015-05-19 15:52:24 +0200

    Cluster analysis algorithms TODO Next: TSP

--- src/Cluster/Cluster/Math/KMeans.cs
+++ src/Cluster/Cluster/Math/KMeans.cs
@@ -16,10 +16,18 @@ namespace Cluster.Math
 
         private int k;
 
+        public int K
+        {
+            get { return k; }
+            private set { k = value; }
+        }
+
+
         private double tol;
 
-        private List<List<int>> cluster_indecies = new List<List<int>>();
-        private Point[] centroids;
+        public List<List<int>> cluster_indecies = new List<List<int>>();
+
+        public Point[] centroids;
 
         private int max_iter;
         /******************************************************************/
@@ -50,6 +58,7 @@ namespace Cluster.Math
 
         private void compute()
         {
+            List<List<int>> cluster_indecies = getNewList();
             // 1) randomly select k centers
             for (int i = 0; i < k; i++)
             {
@@ -57,8 +66,6 @@ namespace Cluster.Math
             }
 
             // 2) group points to their closest centroids
-
-            // for every point
             for (int i = 0; i < data.Count; i++)
             {
                 Point point = data[i];
@@ -79,9 +86,10 @@ namespace Cluster.Math
             }
 
             int iter = 0;
+            // 4) repeat 2. 3. untill convergence
             while (iter++ < max_iter)
             {
-                // 3) Update centers with the mean of all points belonging to the corresponding cluster
+                // 3) 
                 for (int j = 0; j < k; j++)
                 {
                     List<int> cluster = cluster_indecies.ElementAt(j);
@@ -94,7 +102,8 @@ namespace Cluster.Math
                     centroids[j] = Point.mean(cluster_points);
                 }
 
-                // Find new centroids
+                // 2)
+                cluster_indecies = getNewList();
                 for (int i = 0; i < data.Count; i++)
                 {
                     Point point = data[i];
@@ -114,17 +123,45 @@ namespace Cluster.Math
                     cluster_indecies.ElementAt(min_index).Add(i);
                 }
             }
-            // 4) repeat 2. 3. untill convergence
 
+            this.cluster_indecies = cluster_indecies;
         }
 
         private List<List<int>> getNewList()
-        { 
+        {
+            List<List<int>> indecies = new List<List<int>>();
+
+            for (int i = 0; i < k; i++)
+            {
+                List<int> row = new List<int>();
+                indecies.Add(row);
+            }
+            return indecies;
         }
 
 
         /*******************************************************************/
         /************************* PUBLIC METHODS **************************/
         /*******************************************************************/
+
+        public void Compute()
+        {
+            compute();
+        }
+
+        public bool IndexBelongs(int clusterIndex, int pointIndex)
+        {
+            for (int i = 0; i < cluster_indecies[clusterIndex].Count; i++)
+            {
+                if (cluster_indecies[clusterIndex][i] == pointIndex)
+                    return true;
+            }
+            return false;
+        }
+
+        public List<int> GetCluterIndecies(int cluster_index) 
+        {
+            return cluster_indecies[cluster_index];
+        }
     }
 }
--- src/Cluster/TaskManager/TaskSolvers/DVRP/DVRPSolver.cs
+++ src/Cluster/TaskManager/TaskSolvers/DVRP/DVRPSolver.cs
@@ -1,4 +1,5 @@
 ﻿using Cluster.Benchmarks;
+using Cluster.Math;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,22 +31,67 @@ namespace TaskManager.TaskSolvers.DVRP
 
         }
 
-        public override byte[][] DivideProblem(int threadCount)
+        public void FullSolveTest() 
         {
+            /******************* DIVIDE *************************/
             // Static problem data
             VRPParser benchmark = TestCases.Test1();
 
-            // 3d clustering.
-            // 2d relates to coordinates, 3th dim is the time
-            // ommit origins - depot
+            // Combine coords (x, y) and time_avail (z)
+            List<Point> data = new List<Point>();
+            for (int i = 0; i < benchmark.Num_Visits; i++)
+            {
+                List<int> point_coords = new List<int>();
 
+                // does not include depots - which is what we want
+                int loc_index = benchmark.Visit_Location[i];
 
+                point_coords.Add(benchmark.Location_Coord[loc_index][0]);
+                point_coords.Add(benchmark.Location_Coord[loc_index][1]);
 
+                point_coords.Add(benchmark.Time_Avail[loc_index - 1]);
 
-            // cluster evaluation 
+                data.Add(new Point(point_coords));
+            }
 
-            // 
+            // get optimal number of clusters
+            PredictionStrength ps = new PredictionStrength(data);
+            ps.Compute();
+            int k = ps.BestK;
 
+            // compute clusters
+            KMeans clusters = new KMeans(data, k);
+            clusters.Compute();
+
+            // create k benchmarks for k solvers
+            for (int i = 0; i < k; i++) 
+            {
+                int num_depots = benchmark.Num_Depots;
+                VRPParser partial_benchmark = benchmark; // remind your self of references
+
+                int num_visits = clusters.GetCluterIndecies(k).Count;
+                int num_locations = clusters.GetCluterIndecies(k).Count + num_depots;
+
+                int[][] location_coord = new int[num_locations][];
+                location_coord[0] = new int[2];
+                location_coord[0][0] = benchmark.Location_Coord[0][0];
+                location_coord[0][1] = benchmark.Location_Coord[0][1];
+
+                for (int j = num_depots; j < num_locations; j++) 
+                {
+
+                }
+
+            }
+
+            /******************* SOLVE *************************/
+
+            /******************* MERGE *************************/
+        }
+
+        // 1) Divide
+        public override byte[][] DivideProblem(int threadCount)
+        {
             throw new NotImplementedException();
         }
 
